7ef52dd5e8cbadf2b77fbb9ad4abb82ba1f3b039
Samuel Gutierrez <sgutierrezos@unal.edu.co>
2024-03-01 19:52:35 -0500

    primer commit

--- word_count.py
+++ word_count.py
@@ -5,6 +5,7 @@ import glob
 import pandas as pd
 
 
+
 def load_input(input_directory):
     """Load text files in 'input_directory/'"""
     #
@@ -13,6 +14,13 @@ def load_input(input_directory):
     # entrada en el DataFrame.
     #
 
+    filenames = glob.glob(f"{input_directory}/*.txt")
+    dataframes = [
+        pd.read_csv(filename, sep="\t", header=None, names=["text"])
+        for filename in filenames
+    ]
+    concatenated_df = pd.concat(dataframes, ignore_index=True)
+    return concatenated_df
 
 def clean_text(dataframe):
     """Text cleaning"""
@@ -20,13 +28,40 @@ def clean_text(dataframe):
     # Elimine la puntuación y convierta el texto a minúsculas.
     #
 
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.lower()
+    dataframe["text"]= dataframe["text"].str.replace(",","")
+    dataframe["text"]= dataframe["text"].str.replace(".","")
+
+    return dataframe
+
 
 def count_words(dataframe):
     """Word count"""
 
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.split()
+    dataframe = dataframe.explode("text")
+    dataframe["count"] = 1
+
+    dataframe = dataframe.groupby("text", as_index=False).agg({"count":"sum"})
+
+    return dataframe
+
+def count_words_(dataframe):
+    """Word count"""
+
+    dataframe = dataframe.copy()
+    dataframe["text"] = dataframe["text"].str.split()
+    dataframe = dataframe.explode("text")
+    dataframe = dataframe.value_counts()
+
+    return dataframe
+
 
 def save_output(dataframe, output_filename):
     """Save output to a file."""
+    dataframe.to_csv(output_filename, sep="\t", index=True, header=False)
 
 
 #
@@ -34,7 +69,10 @@ def save_output(dataframe, output_filename):
 #
 def run(input_directory, output_filename):
     """Call all functions."""
-
+    df = load_input(input_directory)
+    df = clean_text(df)
+    df = count_words_(df)
+    save_output(df, output_filename)
 
 if __name__ == "__main__":
     run(
